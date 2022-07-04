9f2e04c82097f121e79bfe2f71539d1176c1a0ba
Jennifer Jimenez <jenniferjimenez90@gmail.com>
2022-07-03 20:44:12 -0400

    add project dependencies

---
+++ server.js
@@ -0,0 +1,12 @@
+projectData = {};
+
+const express = require('express');
+
+const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const cors = require('cors');
+app.use(cors());
