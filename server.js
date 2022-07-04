7695146df3a576d60c39dda0ee7d87a409695e83
Jennifer Jimenez <jenniferjimenez90@gmail.com>
2022-07-03 20:53:36 -0400

    initialize main project folder

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.use(bodyParser.json());
 const cors = require('cors');
 app.use(cors());
 
+app.use(express.static('website'));
+
 const port = 8000;
 
 const server = app.listen(port, listening);
